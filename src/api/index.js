31276df7f65f7d3eb3a1a8c9d7a01cbc2ea7893e
vaibhav_vbt <vaibhavtiwade053@gmail.com>
2026-01-16 16:33:15 +0530

    ur changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://scalar-assignment-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
